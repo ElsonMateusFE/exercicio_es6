15cc4e3ec6643a5373412f9a8176744784f30b2b
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2023-01-23 15:45:12 -0300

    exercicio_es6

---
+++ main.js
@@ -0,0 +1,17 @@
+const notasDosAlunos = [
+    { nome: 'Pedro', notas: 10 },
+    { nome: 'João', notas: 7 },
+    { nome: 'Carla', notas: 6 },
+    { nome: 'Maria', notas: 5.6 },
+    { nome: 'José', notas: 9 },
+    { nome: 'Sarah', notas: 5.9 },
+    { nome: 'Giovanna', notas: 8.5 },
+    { nome: 'Karol', notas: 10 },
+    { nome: 'Hugo', notas: 9.2 }
+]
+
+const notasMaiorIgualASeis = notasDosAlunos.filter((item) => {
+    return item.notas >= 6
+})
+
+console.log(notasMaiorIgualASeis)
